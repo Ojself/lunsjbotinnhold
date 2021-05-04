5c18486f1acc32ac8ba3726c6f45227f92cd9617
Tormod Flesjø <tormod@host-37-191-248-127.lynet.no>
2021-05-04 17:19:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
-function App() {
+const EXTERNAL_API = "http://localhost:5000/today";
+const githubUrl = "https://github.com/Ojself/lunsjbot";
+
+console.log(githubUrl);
+
+const Menu = ({ menu }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {menu.map((d) => {
+        return (
+          <div style={{ marginTop: "10vh" }} key={d.name}>
+            <h2>{d.name}</h2>
+            <h4>Allergener</h4>
+            {d.allergens.length ? (
+              d.allergens.map((n) => {
+                return <li key={n.name}>{n.name}</li>;
+              })
+            ) : (
+              <em> Intet oppgitt</em>
+            )}
+            <h4>Næringsinnhold</h4>
+            {d.nutrients.map((n) => {
+              return (
+                <li key={n.name}>
+                  {n.name} - {n.value} {n.code}
+                </li>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
+
+const App = () => {
+  const [menu, setMenu] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchMenu = async () => {
+      let result;
+      try {
+        result = await axios.get(EXTERNAL_API);
+        setLoading(false);
+      } catch (err) {
+        console.warn(err);
+        return;
+      }
+      setMenu(result.data.todaysMenu);
+    };
+    fetchMenu();
+  }, []);
+  return (
+    <div>
+      <header>
+        <h1>LUNSJBOT</h1>
+        <p>
+          Lunsjbot er <a href={githubUrl}>open source</a>
+        </p>
+      </header>
+      {loading || menu.length === 0 ? (
+        <p> Loading ... </p>
+      ) : (
+        <Menu menu={menu} />
+      )}
+    </div>
+  );
+};
 
 export default App;
